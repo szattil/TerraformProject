dfa75908b40ed817e8921fea08586904f5a3f673
Attila <sza.attila1102@gmail.com>
2024-08-14 14:28:55 +0200

    Successful install with install screen

--- main.tf
+++ main.tf
@@ -169,12 +169,12 @@ resource "aws_db_instance" "nextcloud" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
   db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.db.id]
   db_subnet_group_name = aws_db_subnet_group.nextcloud.name
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
-key_name                 = "your-ec2-key-pair-name"
-existing_ec2_instance_id = "i-1234567890abcdef0"  # Replace with your actual instance ID, or remove if not using
-db_username              = "your-db-username"
-db_password              = "your-db-password"
-nextcloud_admin_password = "your-nextcloud-admin-password"
+key_name                 = "workshopkey"
+existing_ec2_instance_id = ""  # Replace with your actual instance ID, or remove if not using
+db_username              = "admin"
+db_password              = "peropero"
+nextcloud_admin_password = "adminpass"
